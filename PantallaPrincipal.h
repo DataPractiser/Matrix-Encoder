0fc99f2ad0f3b3a99fd3a9ecb1e01dc0f881adba
Victor <vicdevelop@protonmail.com>
2020-12-22 14:17:38 -0600

    Archivo PantallaPrincipal.h modificado -Se movio la llamada a la libreria  <wx/taskbar.h> al comienzo del archivo ya que antes estan en condicional.

--- PantallaPrincipal.h
+++ PantallaPrincipal.h
@@ -5,6 +5,7 @@
 
 // For compilers that support precompilation, includes "wx/wx.h".
 #include "wx/wxprec.h"
+#include <wx/taskbar.h>
 //#include "wx/slider.h"
 
 #ifdef __BORLANDC__
@@ -15,10 +16,8 @@
 // need because it includes almost all "standard" wxWidgets headers)
 #ifndef WX_PRECOMP
 #include "wx/wx.h"
-#include "wx/taskbar.h"
-#include "Conexion.h"
 #endif
-
+#include "Conexion.h"
 // ----------------------------------------------------------------------------
 // resources
 // ----------------------------------------------------------------------------
